baf142e64d08099fee8df0e09e0ca6c2eca5f0e0
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-07-16 21:05:38 +0300

    fix

--- WebBlockModel/Implements/WebBlockWaits.cs
+++ WebBlockModel/Implements/WebBlockWaits.cs
@@ -16,9 +16,48 @@ namespace WebBlockModel
         {
             IJavaScriptExecutor js = Browser.Driver as IJavaScriptExecutor;
 
-            RootBlock.Waiter.Until(driver =>
+            Browser.Waiter.Until(driver =>
             {
                 return (bool)js.ExecuteScript("return getAllAngularTestabilities().findIndex(x => !x.isStable()) === -1");
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void WaitJQuery()
+        {
+            IJavaScriptExecutor js = Browser.Driver as IJavaScriptExecutor;
+
+            Browser.Waiter.Until(driver =>
+            {
+                return (bool)js.ExecuteScript("return jQuery.active === 0");
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void WaitAnimation()
+        {
+            IJavaScriptExecutor js = Browser.Driver as IJavaScriptExecutor;
+
+            Browser.Waiter.Until(driver =>
+            {
+                return (bool)js.ExecuteScript("return document.getAnimations().length === 0");
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual void WaitDocumentComplete()
+        {
+            IJavaScriptExecutor js = Browser.Driver as IJavaScriptExecutor;
+
+            Browser.Waiter.Until(driver =>
+            {
+                return (bool)js.ExecuteScript("return document.readyState === \"complete\"");
             });
         }
     }
